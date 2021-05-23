4ad8eb66d902817eb888830813dd71c8e4b6a2be
iampranavdhar <nspr2002@gmail.com>
2021-05-23 16:52:07 +0530

    Removed API part

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,31 +1,31 @@
 import React from 'react'
 import './Contact.css'
-import emailjs from 'emailjs-com';
-import Config from './Config.js';
+// import emailjs from 'emailjs-com';
+// import Config from './Config.js';
 
 function Contact() {
 
-    const {config} = Config();
+    // const {config} = Config();
 
-    const sendEmail = (e) => {
+    // const sendEmail = (e) => {
         
-        e.preventDefault();
-        emailjs.sendForm(config.SERVICE_ID, config.TEMPLATE_ID, e.target, config.SECRET_API_KEY)
-            .then((result) => {
-                console.log(result.text);
-                alert("Your Message has been delivered");
-            }, (error) => {
-                console.log(error.text);
-            });
-            e.target.reset();   
-    }
+    //     e.preventDefault();
+    //     emailjs.sendForm(config.SERVICE_ID, config.TEMPLATE_ID, e.target, config.SECRET_API_KEY)
+    //         .then((result) => {
+    //             console.log(result.text);
+    //             alert("Your Message has been delivered");
+    //         }, (error) => {
+    //             console.log(error.text);
+    //         });
+    //         e.target.reset();   
+    // }
 
     return (
         <div className='contact' id='contact'>
             <h2 className='contact-title'>Contact</h2>
             <div className="contact-container">
                 <img className='contact-image' src='./assets/contact.png' alt=''></img>
-                <form onSubmit={sendEmail}>
+                <form >
                     <label htmlFor="name">Name</label>
                     <input type="text" id="name" name="name" placeholder="Your name.." required/>
 
